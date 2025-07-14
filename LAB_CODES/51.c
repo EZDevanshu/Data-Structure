214b3ca9a24dbe9a0acfe8fbaa47980edfd0af07
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-07-14 18:57:10 +0530

    LabCode

---
+++ LAB_CODES/51.c
@@ -0,0 +1,27 @@
+//WAP to calculate the sum of n numbers using Pointer.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+	printf("Enter the val of n :");
+	int n = 0;
+	scanf("%d" , &n);
+
+	int *ptr = (int *)malloc(n *sizeof(int));
+	
+	printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &ptr[i]);
+    }
+    int sum = 0 ;
+    for (int i = 0; i < n; i++) {
+       sum += ptr[i];
+    }
+
+    printf("sum of the elemets : %d ", sum);
+
+	free(ptr);
+
+}
